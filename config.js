e9b0188e8a9c91149fb10788228da41d935673b3
Ujjwal1001 <64380651+Ujjwal1001@users.noreply.github.com>
2026-02-05 14:23:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,9 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "This Valentine’s week, I’m sending you love in every form I can—
+gifts, smiles, hugs, kisses, and endless thoughts of you.
+You are my favorite place, no matter the distance.",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
